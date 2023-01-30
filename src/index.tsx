d8fc4b2ff76b4825a218e84a0644f0cef67522e2
oleksii <alx.pedchenko@gmail.com>
2023-01-30 22:46:38 +0100

    1.4 React and Scss

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import { render } from 'react-dom'
+import './index.scss'
+
+// const index = () => {
+//   return (
+//     <div>index</div>
+//   )
+// }
+
+render(<div>index</div>, document.getElementById('root'))
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export default function () {
-  console.log('some fn')
-}
-
-// document.body.style.background = 'red'
