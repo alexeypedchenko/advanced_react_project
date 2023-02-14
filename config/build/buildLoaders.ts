23c81caab35881e56f31ac7c0577cf3cafb0bb16
oleksii <alx.pedchenko@gmail.com>
2023-02-14 19:48:25 +0100

    1.7 Babel. Extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,6 +3,26 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        // plugins: [
+        //   [
+        //     'i18next-extract',
+        //     {
+        //       locales: ['en', 'ru'],
+        //       keyAsDefaultValue: true,
+        //     },
+        //   ],
+        // ],
+      },
+    },
+  }
+
   const fileLoader = {
     test: /\.(png|jpe?g|gif)$/i,
     use: [
@@ -45,5 +65,5 @@ export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
     exclude: /node_modules/,
   }
 
-  return [fileLoader, svgLoader, typescriptLoader, scssLoader]
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, scssLoader]
 }
